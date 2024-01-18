b87f7f861ad4bac07c8f13096a7deabd02ed3e3e
wariored <tidjanekonteye@gmail.com>
2024-01-18 06:42:43 +0100

    add permission restriction

--- ecg_analyzer/views.py
+++ ecg_analyzer/views.py
@@ -1,12 +1,16 @@
 from rest_framework import viewsets
+
+from .permissions import IsOwnerOrAdmin
 from .models import ECG, Lead
 from .serializers import ECGSerializer, LeadSerializer
 
+
 class ECGViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrAdmin]
     queryset = ECG.objects.all()
     serializer_class = ECGSerializer
+
 
 class LeadViewSet(viewsets.ModelViewSet):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
-
